427185862461a9cf5b617634565ebcf923ec5c25
semiha yıldız karaderili <semihakaraderili@gmail.com>
2023-05-21 13:06:55 +0300

    temel olaylar öğrenildi ve örnek yapıldı

---
+++ event-dörtlü butonlar/app.js
@@ -0,0 +1,17 @@
+function calis() {
+  alert("siteden çıkış yapmak istediğinize emin misiniz?");
+}
+function gec() {
+  alert("*******üzerine geldiniz******");
+}
+
+function out() {
+  alert("---------üzerinden geçtiniz----");
+}
+
+function klavye() {
+  alert("--------klayve kullandınız----");
+}
+function bitti() {
+  alert("sayfa yüklendi");
+}
